69f0d448944360466e5a275021143233bfdbe090
Christian Stein <sormuras@gmail.com>
2020-12-04 22:26:08 +0100

    Create module-info.java

---
+++ .bach/build/module-info.java
@@ -0,0 +1,15 @@
+import com.github.sormuras.bach.project.ProjectInfo;
+import com.github.sormuras.bach.project.ProjectInfo.ExternalModules;
+import com.github.sormuras.bach.project.ProjectInfo.ExternalModules.Link;
+
+@ProjectInfo(
+      externalModules =
+        @ExternalModules(
+            links = {
+              @Link(module = "se.jbee.inject.lang", to = "https://github.com/jbee/purejin/releases/download/early-access/se.jbee.inject.lang@8-ea.jar")
+            })
+      )
+)
+module build {
+  requires com.github.sormuras.bach;
+}
