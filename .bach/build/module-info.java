be701c8e671c70427dbbb4dee59326bc1d732315
Christian Stein <sormuras@gmail.com>
2020-12-05 10:55:31 +0100

    Fix project-info syntax

--- .bach/build/module-info.java
+++ .bach/build/module-info.java
@@ -3,12 +3,16 @@ import com.github.sormuras.bach.project.ProjectInfo.ExternalModules;
 import com.github.sormuras.bach.project.ProjectInfo.ExternalModules.Link;
 
 @ProjectInfo(
-      externalModules =
+    name = "bach-purejin",
+    externalModules =
         @ExternalModules(
             links = {
-              @Link(module = "se.jbee.inject.lang", to = "https://github.com/jbee/purejin/releases/download/early-access/se.jbee.inject.lang@8-ea.jar")
-            })
-      )
+              @Link(
+                  module = "se.jbee.inject.lang",
+                  to =
+                      "https://github.com/jbee/purejin/releases/download/early-access/se.jbee.inject.lang@8-ea.jar")
+            }
+        )
 )
 module build {
   requires com.github.sormuras.bach;
